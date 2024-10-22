587938c1a5d96fedcce38ca64573226c5b18b6ae
Aleksandr <sasha.syrota15@gmail.com>
2024-10-22 12:06:26 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,36 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word, result",
+    [
+        pytest.param(
+            "HeLlo",
+            False,
+            id="test should return False for `HeLlo`"
+        ),
+        pytest.param(
+            "lumberjacks",
+            True,
+            id="test should return True for `lumberjacks`"
+        ),
+        pytest.param(
+            "loving",
+            True,
+            id="test should return True for `loving`"
+        ),
+        pytest.param(
+            " Jam ",
+            False,
+            id="test should return False for `Jam `"
+        ),
+        pytest.param(
+            "",
+            True,
+            id="test should return True for empty string"
+        )
+    ]
+)
+def test_func_should_correctly_return_result(word: str, result: bool) -> bool:
+    assert is_isogram(word) == result
